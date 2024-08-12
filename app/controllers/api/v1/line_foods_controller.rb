96361fa10684995845f7316e004246bf848ff5ad
Pen Peen <ikegami1022.h@gmail.com>
2024-08-12 19:56:55 +0900

    :sparkles: replace

--- app/controllers/api/v1/line_foods_controller.rb
+++ app/controllers/api/v1/line_foods_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class LineFoodsController < ApplicationController
-      before_action :set_line_food, only: %i[create]
+      before_action :set_line_food, only: %i[create replace]
       before_action :validate_ordered, only: %i[create]
 
       def index
@@ -28,6 +28,24 @@ module Api
           }, status: :created
         else
           head :unprocessable_entity
+        end
+      end
+
+      def replace
+        ActiveRecord::Base.transaction do
+          LineFood.active.other_restaurant(@ordered_food.restaurant.id).each do |line_food|
+            line_food.update!(:active, false)
+          end
+
+          set_line_food(@ordered_food)
+
+          if @line_food.save!
+            render json: {
+              line_food: @line_food
+            }, status: :created
+          end
+        rescue
+          head :internal_server_error
         end
       end
 
