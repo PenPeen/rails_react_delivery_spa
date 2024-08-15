198d821acffcbb0dd59992d7223d9978733f0141
Pen Peen <ikegami1022.h@gmail.com>
2024-08-15 10:45:34 +0900

    :sparkles: Foodモーダルで注文数が変更できるように

--- frontend/src/components/FoodModal/FoodModal.tsx
+++ frontend/src/components/FoodModal/FoodModal.tsx
@@ -1,8 +1,10 @@
 import Modal from "../Modal/ModalContainer";
-import styled from "./foods.module.css";
+import styles from "./foods.module.css";
 import OrderHeaderImage from "@/assets/order-header.png";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ParentContext } from "@/App";
+import { CountDownButton } from "@/components/CountDownButton/CountDownButton";
+import { CountUpButton } from "@/components/CountUpButton/CountUpButton";
 
 type FoodModalProps = {
   showModal: boolean;
@@ -16,11 +18,26 @@ export const FoodModal: React.FC<FoodModalProps> = ({
   handleCloseModal,
 }) => {
   const [selectedFood] = useContext(ParentContext);
+  const [sales, setSales] = useState(1);
+
+  const countUp = () => {
+    setSales((count) => ++count);
+  };
+  const countDown = () => {
+    setSales((count) => --count);
+  };
+
+  const countUpIsDisabled = () => sales >= 10;
+  const countDownIsDisabled = () => sales <= 1;
+
+  const submitOrder = () => {
+    // TODO: オーダーアクション
+  };
 
   return (
     <>
       {selectedFood && (
-        <div className={styled.food_modal}>
+        <div className={styles.food_modal}>
           <Modal
             showModal={showModal}
             handleOpenModal={handleOpenModal}
@@ -30,19 +47,39 @@ export const FoodModal: React.FC<FoodModalProps> = ({
             isOpenButton={false}
           >
             <img
-              className={styled.food_modal__header_image}
+              className={styles.food_modal__header_image}
               src={OrderHeaderImage}
               alt='order header'
             />
             <h3>{selectedFood.name}</h3>
             <div>
-              <div className={styled.food_modal__description_wrapper}>
-                <p className={styled.food_modal__description_subtext}>
+              <div className={styles.food_modal__description_wrapper}>
+                <p className={styles.food_modal__description_subtext}>
                   {selectedFood.description}
                 </p>
               </div>
             </div>
-            <div>数量</div>
+            <div className={styles.food_modal__sales_wrapper}>
+              <div className={styles.food_modal__sales_count_wrapper}>
+                <CountDownButton
+                  size='large'
+                  handleClick={countDown}
+                  isDisabled={countDownIsDisabled()}
+                />
+                <div>{sales}</div>
+                <CountUpButton
+                  size='large'
+                  handleClick={countUp}
+                  isDisabled={countUpIsDisabled()}
+                />
+              </div>
+              <button
+                className={styles.food_modal__order_button}
+                onClick={submitOrder}
+              >
+                注文に{sales}個追加する・¥{selectedFood.price * sales}
+              </button>
+            </div>
           </Modal>
         </div>
       )}
