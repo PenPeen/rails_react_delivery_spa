3a53e11ad98b8b0eef3716b422052b194951ec41
Pen Peen <ikegami1022.h@gmail.com>
2024-08-16 17:14:06 +0900

    :art: Headerを表示する

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Foods, Orders, Restaurants } from "./components";
-import { createContext, Dispatch, SetStateAction, useState } from "react";
+import { createContext, Dispatch, SetStateAction } from "react";
 import { Food } from "./type/food";
+import { Header } from "./components/Header/Header";
 
 type ParentContextType = [Food, Dispatch<SetStateAction<Food | null>>];
 export const ParentContext = createContext<ParentContextType>({} as never);
@@ -11,7 +12,9 @@ function App() {
   return (
     <>
       <BrowserRouter>
+        <Header title='PenEats' logoUrl='/logo.jpg' />
         <Routes>
+          <Route path='/' element={<Restaurants />} />
           <Route path='/restaurants' element={<Restaurants />} />
           <Route path='/restaurants/:restaurantsId/foods' element={<Foods />} />
           <Route path='/orders' element={<Orders />} />
