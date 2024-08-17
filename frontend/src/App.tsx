cda7e52df5a8fea41c8a4e64638b7ac4c43bbb00
Pen Peen <ikegami1022.h@gmail.com>
2024-08-17 09:36:25 +0900

    :sparkles: useContextを使用してグローバルに値を管理する

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,28 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Foods, Orders, Restaurants } from "./components";
 import { Header } from "./components/Header/Header";
+import { createContext, useReducer } from "react";
+import CartSlice, { initialState } from "./stores/cart_reducer";
+
+type CartContextType = [
+  number,
+  (count: number) => void,
+  (count: number) => void
+];
+export const CartContext = createContext<CartContextType>({} as never);
+const { actions, reducer } = CartSlice;
+const { add, remove } = actions;
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const handleAdd = (count: number) => dispatch(add(count));
+  const handleRemove = (count: number) => dispatch(remove(count));
+
   return (
     <>
+      <CartContext.Provider
+        value={[state.count, handleAdd, handleRemove]}
+      ></CartContext.Provider>
       <BrowserRouter>
         <div className='head__contents'>
           <Header title='PenEats' logoUrl='/logo.jpg' isFixed />
