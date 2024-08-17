5b3f94fb0ba30012a879dbe862c976ae7b879d3b
Pen Peen <ikegami1022.h@gmail.com>
2024-08-17 09:26:24 +0900

    :construction: 注文画面の作成

--- frontend/src/components/Orders/Orders.tsx
+++ frontend/src/components/Orders/Orders.tsx
@@ -1,3 +1,27 @@
+import { lineFoods } from "@/config/constants";
+import { useRequestStatus } from "@/hooks/use_request_status";
+import { LineFood } from "@/type/line_food";
+import ApiClient from "@/utils/api-client";
+import { useEffect, useState } from "react";
+
 export const Orders: React.FC = () => {
-  return <>注文画面</>;
+  const { state, fetching, success } = useRequestStatus();
+  const [orderFoods, setOrderFoods] = useState<LineFood>();
+
+  useEffect(() => {
+    fetching();
+
+    const client = new ApiClient();
+    client.get(lineFoods).then((data) => {
+      setOrderFoods(data.line_food_ids);
+      success();
+    });
+  }, []);
+
+  return (
+    <>
+      注文画面
+      {orderFoods ? <div>注文あります。</div> : <div>...</div>}
+    </>
+  );
 };
