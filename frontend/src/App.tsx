947b5d5f5bd543d770605367962b60e9d805df86
Pen Peen <ikegami1022.h@gmail.com>
2024-08-17 09:29:49 +0900

    :fire: 不要な記述の削除

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Foods, Orders, Restaurants } from "./components";
-import { createContext, Dispatch, SetStateAction } from "react";
-import { Food } from "./type/food";
 import { Header } from "./components/Header/Header";
-
-type ParentContextType = [Food, Dispatch<SetStateAction<Food | null>>];
-export const ParentContext = createContext<ParentContextType>({} as never);
 
 function App() {
   return (
