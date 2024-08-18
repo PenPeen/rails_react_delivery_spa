4a0906646852b418db3e4c715eaf4c674c498d03
Pen Peen <ikegami1022.h@gmail.com>
2024-08-18 19:09:12 +0900

    :recycle: replaceメソッドの仕様修正

--- app/controllers/api/v1/line_foods_controller.rb
+++ app/controllers/api/v1/line_foods_controller.rb
@@ -5,7 +5,7 @@ module Api
       before_action :validate_ordered, only: %i[create]
 
       def index
-        line_foods = LineFood.active
+        line_foods = LineFood.all
 
         if line_foods.present?
           render json: {
@@ -25,7 +25,7 @@ module Api
         if @line_food.save
           render json: {
             line_food: @line_food,
-            count: active_food_sum
+            count: food_sum
           }, status: :created
         else
           head :unprocessable_entity
@@ -34,16 +34,14 @@ module Api
 
       def replace
         ActiveRecord::Base.transaction do
-          LineFood.active.other_restaurant(@ordered_food.restaurant.id).each do |line_food|
-            line_food.update!(active: false)
-          end
+          LineFood.other_restaurant(@ordered_food.restaurant.id).each(&:destroy!)
 
           set_line_food(@ordered_food)
 
           if @line_food.save!
             render json: {
               line_food: @line_food,
-              count: active_food_sum
+              count: food_sum
             }, status: :created
           end
         rescue
@@ -53,7 +51,7 @@ module Api
 
       def cart_count
         render json: {
-          count: active_food_sum
+          count: food_sum
         }, status: :ok
       end
 
@@ -62,12 +60,12 @@ module Api
           @ordered_food = Food.find(params[:food_id])
         end
 
-        def active_food_sum
-          LineFood.active.sum(:count)
+        def food_sum
+          LineFood.sum(:count)
         end
 
         def validate_ordered
-          if LineFood.active.other_restaurant(@ordered_food.restaurant.id).exists?
+          if LineFood.other_restaurant(@ordered_food.restaurant.id).exists?
             render json: {
               existing_restaurant: LineFood.other_restaurant(@ordered_food.restaurant.id).first.restaurant.name,
               new_restaurant: Food.find(params[:food_id]).restaurant.name
@@ -79,14 +77,12 @@ module Api
           if ordered_food.line_food.present?
             @line_food = ordered_food.line_food
             @line_food.attributes = {
-              count: ordered_food.line_food.count + params[:count],
-              active: true
+              count: ordered_food.line_food.count + params[:count]
             }
           else
             @line_food = ordered_food.build_line_food(
               count: params[:count],
               restaurant: ordered_food.restaurant,
-              active: true
             )
           end
         end
