b659f28290060bda998566193c059ba7d882b656
Pen Peen <ikegami1022.h@gmail.com>
2024-08-21 08:27:06 +0900

    :zap: Buttonコンポーネントの拡張

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -9,6 +9,7 @@ interface ButtonProps {
   label: string;
   isRadius?: boolean;
   isSolid?: boolean;
+  isFull?: boolean;
   isDisabled?: boolean;
   handleClick?: () => void;
 }
@@ -19,18 +20,20 @@ export const Button: React.FC<ButtonProps> = ({
   label,
   isRadius = false,
   isSolid = false,
+  isFull = false,
   isDisabled = false,
   handleClick,
 }: ButtonProps) => {
   const mode = styles[`a_button__${type}`];
   const solid = isSolid && styles[`a_button__${type}_solid`];
   const radius = isRadius && styles.a_button__radius;
+  const full = isFull && styles.a_button__full;
 
   return (
     <button
       type="button"
       disabled={isDisabled}
-      className={[styles.a_button, styles[`a_button__${size}`], mode, solid, radius].join(' ')}
+      className={[styles.a_button, styles[`a_button__${size}`], mode, solid, radius, full].join(' ')}
       onClick={handleClick}
     >
       {label}
