6f5b983cdc2c698395cca9e8c752ccff2a691eae
Pen Peen <ikegami1022.h@gmail.com>
2024-09-02 08:06:07 +0900

    :wrench: devise設定のカスタマイズ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for "User", at: "auth"
-
   namespace :api do
     namespace :v1 do
       resources :restaurants do
@@ -11,6 +9,14 @@ Rails.application.routes.draw do
       put "line_foods/replace", to: "line_foods#replace"
       get "line_foods/cart_count", to: "line_foods#cart_count"
       resources :orders, only: %i[create]
+
+      mount_devise_token_auth_for "User", at: "auth", controllers: {
+        registrations: "api/v1/sessions"
+      }
+
+      namespace :auth do
+        resources :sessions, only: %i[index]
+      end
     end
   end
 end
