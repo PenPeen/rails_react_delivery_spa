d356ae58c4d815a9f4c7c7c4ba2a2f261b7bbee8
Pen Peen <ikegami1022.h@gmail.com>
2024-09-02 23:00:41 +0900

    :sparkles: 認証用のAPIリクエスト機能の実装

---
+++ frontend/src/utils/auth.ts
@@ -0,0 +1,37 @@
+import { SignInParams, SignUpParams } from '@/type';
+import Cookies from 'js-cookie';
+import ApiClient from './api-client';
+
+export const signUp = (params: SignUpParams) => {
+  const client = new ApiClient();
+  return client.post('auth', params);
+};
+
+export const signIn = (params: SignInParams) => {
+  const client = new ApiClient();
+  return client.post('auth/sign_in', params);
+};
+
+export const signOut = () => {
+  const client = new ApiClient();
+  return client.delete('auth/sign_out', {
+    headers: {
+      'access-token': Cookies.get('_access_token'),
+      client: Cookies.get('_client'),
+      uid: Cookies.get('_uid'),
+    },
+  });
+};
+
+export const getCurrentUser = () => {
+  const client = new ApiClient();
+
+  if (!Cookies.get('_access_token') || !Cookies.get('_client') || !Cookies.get('_uid')) return;
+  return client.get('/auth/sessions', {
+    headers: {
+      'access-token': Cookies.get('_access_token'),
+      client: Cookies.get('_client'),
+      uid: Cookies.get('_uid'),
+    },
+  });
+};
