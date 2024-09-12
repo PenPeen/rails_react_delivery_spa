020f0d2ee7e729cf8a9f9bf4c37e7a0b27963336
Pen Peen <ikegami1022.h@gmail.com>
2024-09-13 07:57:44 +0900

    :wrench: axiosのデフォルト設定を更新した。

--- frontend/src/utils/api-client.ts
+++ frontend/src/utils/api-client.ts
@@ -1,20 +1,35 @@
-import axios, { AxiosRequestConfig } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+
+function applyCaseMiddleware(axiosInstance: AxiosInstance): AxiosInstance {
+  axiosInstance.interceptors.request.use((config) => {
+    return config;
+  });
+
+  return axiosInstance;
+}
 
 class ApiClient {
+  private axiosInstance: AxiosInstance;
+
+  constructor(baseURL: string = 'http://localhost:3001') {
+    const instance = axios.create({ baseURL });
+    this.axiosInstance = applyCaseMiddleware(instance);
+  }
+
   get(path: string, data?: AxiosRequestConfig) {
-    return axios.get(path, data).then((res) => res.data);
+    return this.axiosInstance.get(path, data).then((res) => res.data);
   }
 
   post<T>(path: string, data: T) {
-    return axios.post(path, data).then((res) => res.data);
+    return this.axiosInstance.post(path, data).then((res) => res.data);
   }
 
   put<T>(path: string, data: T) {
-    return axios.put(path, data).then((res) => res.data);
+    return this.axiosInstance.put(path, data).then((res) => res.data);
   }
 
   delete(path: string, data?: AxiosRequestConfig) {
-    return axios.delete(path, data).then((res) => res.data);
+    return this.axiosInstance.delete(path, data).then((res) => res.data);
   }
 }
 
